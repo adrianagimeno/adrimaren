110dd6c88b27f9fa7ab18fad1a26aec663f9b837
Maren828 <maren.clapers01@estudiant.upf.edu>
2023-11-23 09:58:49 +0100

    test
    jksghjivlhgrququ

--- myTeam.py
+++ myTeam.py
@@ -19,7 +19,7 @@
 # purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
 # John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
-
+# aghabhj
 import random
 import util
 
